a3c183549222592495b2a7505259c33af189599f
shahriarmehedi <sau.shahriar@gmail.com>
2022-06-26 16:53:46 +0600

    hover animation added

--- src/components/LatestEpisodes/LatestEpisodes.jsx
+++ src/components/LatestEpisodes/LatestEpisodes.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const LatestEpisodes = () => {
 
@@ -18,12 +18,21 @@ const LatestEpisodes = () => {
 
     return (
         <div className='w-5/6 mx-auto pb-10'>
-            <h1 className='py-5 text-2xl font-semibold'>Latest Episodes</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 cursor-pointer'>
+            <h1 className='py-5 text-2xl font-semibold text-white'>Latest Episodes</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 cursor-pointer'>
                 {latestEpisodes.map(episode => (
                     <div key={episode.id}>
-                        <img className='mx-auto opacity-70' src={`https://anime-python-backend.herokuapp.com${episode.image}`} alt={episode.title} />
-                        <h2 className='py-2 text-center text-xl font-semibold'>{episode.title} | {episode.episode}</h2>
+                        <Link to="/anime">
+                            <div className="hover:scale-110  relative flex justify-center items-center transition duration-300 hover:text-white">
+                                <img className='mx-auto hover:opacity-90 transition duration-300 opacity-70 border-[3px] shadow rounded-md border-white' src={`https://anime-python-backend.herokuapp.com${episode.image}`} alt={episode.title} />
+                                <button className="absolute">
+                                    <i class="bi bi-play-circle-fill text-4xl shadow-lg opacity-80 text-white"></i>
+                                </button>
+                            </div>
+                        </Link>
+
+
+                        <h2 className='py-2 text-center  font-semibold'>{episode.title}</h2>
                     </div>
                 ))}
             </div>
--- src/components/RandomAnime/RandomAnime.jsx
+++ src/components/RandomAnime/RandomAnime.jsx
@@ -1,29 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const RandomAnime = () => {
-    // const latestEpisodes = [
-    //     {
-    //         id: 1,
-    //         title: 'Eposide 1',
-    //         image: 'https://api.lorem.space/image/movie?w=200&h=280',
-    //     },
-    //     {
-    //         id: 2,
-    //         title: 'Eposide 2',
-    //         image: 'https://api.lorem.space/image/movie?w=200&h=280',
-    //     },
-    //     {
-    //         id: 3,
-    //         title: 'Eposide 3',
-    //         image: 'https://api.lorem.space/image/movie?w=200&h=280',
-    //     },
-    //     {
-    //         id: 4,
-    //         title: 'Eposide 4',
-    //         image: 'https://api.lorem.space/image/movie?w=200&h=280',
-    //     }
 
-    // ];
     const [animes, setAnimes] = useState([]);
 
     useEffect(() => {
@@ -38,13 +17,19 @@ const RandomAnime = () => {
 
     return (
         <div className='w-5/6 mx-auto pb-10'>
-            <h1 className='py-5 text-2xl font-semibold'>Random Animes</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 cursor-pointer'>
+            <h1 className='py-5 text-2xl font-semibold text-white'>Random Animes</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 cursor-pointer'>
                 {animes.map(anime => (
                     <div key={anime.id}>
-
-                        <img className='mx-auto opacity-70' src={`https://anime-python-backend.herokuapp.com${anime.image}`} alt={anime.title} />
-                        <h2 className='py-2 text-center text-xl font-semibold'>{anime.title}</h2>
+                        <Link to="/anime">
+                            <div className="hover:scale-110  relative flex justify-center items-center transition duration-300 hover:text-white">
+                                <img className='mx-auto hover:opacity-90 transition duration-300 opacity-70 border-[3px] shadow rounded-md border-white' src={`https://anime-python-backend.herokuapp.com${anime.image}`} alt={anime.title} />
+                                <button className="absolute">
+                                    <i class="bi bi-play-circle-fill text-4xl shadow-lg opacity-80 text-white"></i>
+                                </button>
+                            </div>
+                        </Link>
+                        <h2 className='py-2 text-center font-semibold'>{anime.title}</h2>
                     </div>
                 ))}
             </div>
